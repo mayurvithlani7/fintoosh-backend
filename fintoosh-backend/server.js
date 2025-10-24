920f2f8ec93d92e56081c674bd016b407a60a43e
Mayur <mayurvithlani7@gmail.com>
2025-10-24 13:42:35 +0530

    Fix bufferCommands error - remove bufferCommands: false
    - Remove bufferCommands: false to allow operations to buffer until MongoDB connection is established
    - This fixes 'Cannot call users.findOne() before initial connection' error
    - Operations will now queue properly during server startup

--- fintoosh-backend/server.js
+++ fintoosh-backend/server.js
@@ -138,7 +138,6 @@ const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/kid-budge
 mongoose.connect(mongoURI, {
   serverSelectionTimeoutMS: 30000, // Keep trying to send operations for 30 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  bufferCommands: false, // Disable mongoose buffering
 });
 
 // Connection event handlers
